d4381f6d81bb321904be4c2152a12f277f1f57ff
Zhlobo Dmitry <dmitry.zhlobo@yandex.ru>
2021-11-27 11:49:22 +0300

    Second

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ import datetime
 
 
 def logger(function_to_log):
-
     def wrapper(*args, **kwargs):
         now = str(datetime.datetime.now())
         name = function_to_log.__name__
+
         returned = function_to_log(*args, **kwargs)
 
         with open('log.txt', 'a') as logfile:
             logfile.write(f'{now}, function: {name}, args: {args}, kwargs: {kwargs}, return: {returned};\n')
+        return returned
 
     return wrapper
+
+
+def log_to(path):
+    def decor(function_to_log):
+        def wrapper(*args, **kwargs):
+            now = str(datetime.datetime.now())
+            name = function_to_log.__name__
+
+            returned = function_to_log(*args, **kwargs)
+
+            with open(path, 'a') as logfile:
+                logfile.write(f'{now}, function: {name}, args: {args}, kwargs: {kwargs}, return: {returned};\n')
+            return returned
+
+        return wrapper
+
+    return decor
 
 
 @logger
@@ -25,5 +43,12 @@ def another_func():
     print('вторая функция(ничего не возвращает и без аргументов)')
 
 
+@log_to(path='other.log')
+def some_func_to_log(first, name='name'):
+    print('первая функция(с агрументами, возвращает 1)')
+    return first, name
+
+
 some_func(first='tets')
 another_func()
+some_func_to_log(first='test', name='some_name')
